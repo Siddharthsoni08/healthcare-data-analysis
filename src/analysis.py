1282f839879bebb9032448ac50d992437d9338ab
Siddharth Soni <siddharthsoni0998202@gmail.com>
2026-03-30 18:21:56 +0530

    Basic EDA completed with visualizations

--- src/analysis.py
+++ src/analysis.py
@@ -22,6 +22,12 @@ print(df.describe())
 print("\nDuplicate rows:")
 print(df.duplicated().sum())
 
+
+# Remove duplicates
+df = df.drop_duplicates()
+
+print("\nAfter removing duplicates:", df.shape)
+
 # Convert names to proper format
 df["Name"] = df["Name"].str.title()
 
@@ -38,3 +44,61 @@ df = df[df["Billing Amount"] > 0]
 
 print("\nCleaned Data Info:")
 print(df.info())
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(style="whitegrid")
+
+# -------------------------------
+# 1. Gender Distribution
+# -------------------------------
+print("\nGender Count:")
+print(df["Gender"].value_counts())
+
+
+# -------------------------------
+# 2. Disease Distribution
+# -------------------------------
+print("\nMedical Condition Count:")
+print(df["Medical Condition"].value_counts())
+
+
+# -------------------------------
+# 3. Admission Type Distribution
+# -------------------------------
+print("\nAdmission Type Count:")
+print(df["Admission Type"].value_counts())
+
+
+# -------------------------------
+# GRAPHS
+# -------------------------------
+
+# Gender Plot
+sns.countplot(x="Gender", data=df)
+plt.title("Gender Distribution")
+plt.savefig("../images/gender_distribution.png")
+plt.show()
+
+
+# Disease Plot
+sns.countplot(y="Medical Condition", data=df)
+plt.title("Disease Distribution")
+plt.savefig("../images/disease_distribution.png")
+plt.show()
+
+
+# Admission Type Plot
+sns.countplot(x="Admission Type", data=df)
+plt.title("Admission Type Distribution")
+plt.savefig("../images/admission_type.png")
+plt.show()
+
+
+# Age Distribution
+sns.histplot(df["Age"], bins=20)
+plt.title("Age Distribution")
+plt.savefig("../images/age_distribution.png")
+plt.show()
